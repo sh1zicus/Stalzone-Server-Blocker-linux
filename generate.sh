219cfbeaaa9d715f5a3db58f75de9c7f0fa64a47
daen2772 <87767572+sh1zicus@users.noreply.github.com>
2026-06-30 21:14:59 +0700

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-SERVERS="$HOME/Stalcraft-Server-Blocker-linux/Servers.json"
+SERVERS="$HOME/Stalzone-Server-Blocker-linux/Servers.json"
 OUT="stalcraft.nft"
 
 cat > "$OUT" <<'EOF'
